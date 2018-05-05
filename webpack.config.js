2dbf06598ba68b8fc58e4df9bea637c6a4063421
deathart <deathart@hotmail.fr>
2018-05-05 14:49:11 +0200

    Fix & typo

--- webpack.config.js
+++ webpack.config.js
@@ -8,13 +8,13 @@ Encore
     //Clean olds files
     .cleanupOutputBeforeBuild()
     //JS
-    .addEntry('js/vendor/scroll', './Resources/assets/js/vendor/scroll.min.js')
-    .addEntry('js/forum/app', './Resources/assets/js/forum/app.js')
-    .addEntry('js/forum/auth', './Resources/assets/js/forum/auth.js')
+    .addEntry('js/vendor/scroll', './resources/assets/js/vendor/scroll.min.js')
+    .addEntry('js/forum/app', './resources/assets/js/forum/app.js')
+    .addEntry('js/forum/auth', './resources/assets/js/forum/auth.js')
     //CSS
-    .addStyleEntry('css/forum/style', './Resources/assets/scss/forum/style.scss')
-    .addStyleEntry('css/vendor/bootstrap', './Resources/assets/scss/bootstrap.scss')
-    .addStyleEntry('css/vendor/font-awesome', './Resources/assets/scss/font-awesome/font-awesome.scss')
+    .addStyleEntry('css/forum/style', './resources/assets/scss/forum/style.scss')
+    .addStyleEntry('css/vendor/bootstrap', './resources/assets/scss/bootstrap.scss')
+    .addStyleEntry('css/vendor/font-awesome', './resources/assets/scss/font-awesome/font-awesome.scss')
     //Add public
     .addPlugin(new webpack.ProvidePlugin({ Cookies: 'js-cookie/src/js.cookie.js' }))
     //Add shared
